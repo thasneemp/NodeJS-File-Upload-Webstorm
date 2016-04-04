a6e5bd98503c34b63ceed93576ede61b38785181
Muhammed Thasneem <muhammed.thasneem@cabotsolutions.com>
2016-04-04 17:44:42 +0530

    multer added

--- NodeJsFileIpload/routes/index.js
+++ NodeJsFileIpload/routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.sendfile('./views/fileupload.html');
+});
+router.post('/upload', function (req, res, next) {
+    //console.log(req.files[0]);
+    //res.redirect("/show?photo=" + req.files[0].filename);
 });
 
+router.get('/show', function (req, res, next) {
+    var imgurl = req.param("photo");
+    res.send("<html><body><img src=" + "../uploads/" + imgurl + "></body></html>");
+
+});
 module.exports = router;
